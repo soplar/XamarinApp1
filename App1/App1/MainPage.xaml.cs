fb55694baf9a9b1426bbeb0a4e5202d5451ebc58
soplar <joan.vanduren@gmail.com>
2018-04-19 13:48:12 +0200

    init

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+        static string bomb = new Random().Next(1, 4).ToString();
+        int scores;
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            // game over
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb exploded", "Game over", "Retry");
+                bomb = new Random().Next(1, 4).ToString();
+                scores = 0;
+            }
+            else
+            {
+                scores += 1;
+                await DisplayAlert("Bomb defused", "Scores: " + scores, "continue");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        }
+	}
+}
